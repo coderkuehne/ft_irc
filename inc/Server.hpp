8fdef4f12e50b12b04d992d936091f8f3519b263
Niimphu <nimphu@outlook.com>
2024-04-08 18:31:11 +0200

    tidy

--- inc/Server.hpp
+++ inc/Server.hpp
@@ -1,41 +1,7 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Server.hpp                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: kekuhne <kekuhne@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/04 13:13:15 by kekuhne           #+#    #+#             */
-/*   Updated: 2024/04/07 18:09:46 by kekuhne          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef SERVER_HPP
 # define SERVER_HPP
 
-# include <iostream>
-# include <string>
-# include <cstring>
-# include <unistd.h>
-# include <sys/socket.h>
-# include <arpa/inet.h>
-# include <exception>
-# include <fcntl.h>
-# include <vector>
-# include <netinet/in.h>
-# include <csignal>
-# include <poll.h>
-# include <cstdlib>
-
-//-------------------------------------------------------//
-#define RED "\033[31m"
-#define GREEN "\033[32m"
-#define YELLOW "\033[33m"
-#define RESET "\033[0m"
-//-------------------------------------------------------//
-
-# define BUFFER_SIZE 1024
-# define PORT 6969
+# include "IRC.hpp"
 
 class Client;
 
