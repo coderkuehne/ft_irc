0cd7c89217315627c0fedd5f6e3e7e2ad69c6b8b
Niimphu <nimphu@outlook.com>
2024-04-09 17:01:53 +0200

    added nickname, username, removed ip, remade constructor

--- inc/Client.hpp
+++ inc/Client.hpp
@@ -5,19 +5,18 @@
 class Client {
 	private:
 		int			_socket;
-		std::string	_ip;
+//		std::string	_ip;
 		std::string	_nick;
 		std::string	_username;
 
 	public:
-		Client() { _socket = 0; _ip = ""; };
-		Client(int socket, struct sockaddr_in addr) { _socket = socket; _ip = inet_ntoa(addr.sin_addr); };
+		Client(int socket): _socket(socket), _nick(""), _username("")  {};
+//		Client(int socket, struct sockaddr_in addr) { _socket = socket; _ip = inet_ntoa(addr.sin_addr); };
 		~Client() {};
 
-		void setSocket(int socket) { _socket = socket; };
 		int getSocket() { return _socket; };
-		void setIp(std::string ip) { _ip = ip; };
-		std::string getIp() { return _ip; };
+//		void setIp(std::string ip) { _ip = ip; };
+//		std::string getIp() { return _ip; };
 
 };
 
