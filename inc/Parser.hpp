e528fcbbaae08531bfb25f98a16d43b52862f4e4
Niimphu <nimphu@outlook.com>
2024-04-11 17:41:26 +0200

    parser header

---
+++ inc/Parser.hpp
@@ -0,0 +1,11 @@
+#ifndef PARSER_HPP
+# define PARSER_HPP
+
+# define NO_PASS 20
+# define NO_DELIM 21
+# define NO_PARAM 22
+
+int		getPassword(std::string, std::string&);
+void	getNames(std::string, std::string&, std::string&);
+
+#endif
